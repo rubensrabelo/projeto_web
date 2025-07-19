72b7615e7ccf9223655bfc222e477d992790c514
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-07-19 08:05:40 -0300

    fix(home): show discipline time on home page

--- frontend/assets/js/home.js
+++ frontend/assets/js/home.js
@@ -8,25 +8,36 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   try {
     const res = await fetch("http://localhost:3000/courses/teacher", {
-      method: "GET",
       headers: {
         Authorization: `Bearer ${token}`
       }
     });
 
-    if (!res.ok) throw new Error("Erro ao buscar disciplinas.");
+    if (!res.ok) {
+      throw new Error("Erro ao buscar disciplinas.");
+    }
 
-    const disciplinas = await res.json();
+    const cursos = await res.json();
     const lista = document.getElementById("listaDisciplinas");
-    lista.innerHTML = "";
 
-    disciplinas.forEach(d => {
+    if (cursos.length === 0) {
+      lista.innerHTML = "<li>Nenhuma disciplina encontrada.</li>";
+      return;
+    }
+
+    console.log(cursos[0])
+
+    cursos.forEach(curso => {
       const li = document.createElement("li");
-      li.textContent = d.name;
+      li.innerHTML = `
+        <strong>${curso.name}</strong><br/>
+        <span>${curso.classSchedule || "Sem horário definido"}</span>
+      `;
       lista.appendChild(li);
     });
+
   } catch (err) {
-    alert("Erro ao carregar disciplinas.");
+    alert("Erro: " + err.message);
   }
 });
 
